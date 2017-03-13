5681654cee45c6eaff7a27198fa6a974d33baabd
Luca <l.calcado@outlook.com>
2017-03-13 09:09:34 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = angular.module('guess', ['angular-timeline']);
+
+app.controller('GuessController', function GuessController($scope,$timeout) {
+    $scope.counter = 0;
+    $scope.onTimeout = function(){
+        $scope.counter++;
+        mytimeout = $timeout($scope.onTimeout,1000);
+    }
+
+    $scope.verify = function () {
+        $scope.difference = $scope.original - $scope.user;
+        $scope.tries = $scope.tries + 1;
+    }
+    $scope.initializeGame = function () {
+        $scope.tries = 0;
+        $scope.original = Math.floor((Math.random() * 1000) + 1);
+        $scope.user = null;
+        $scope.difference = null;
+    }
+    $scope.reloadRoute = function() {
+    	location.reload();
+	}
+
+    $scope.addEvent = function() {
+    if ($scope.difference > 0) {x = 'lower';}
+    else if ($scope.difference < 0){x = 'higher';}
+    else {x = "Win";}
+	$scope.events.push({
+		badgeClass: 'info',
+		badgeIconClass: 'glyphicon-check',
+		title: $scope.user,
+		when: $scope.counter,
+		content: x
+		});
+	}
+	$scope.events = [ ];
+    $scope.initializeGame();
+});
+
